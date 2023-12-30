22fffcf9d55845dffec05271419e4eca28d3b7c8
Om Khulbe <84437272+yashsharma2912@users.noreply.github.com>
2023-12-30 23:46:19 +0530

    fragments

---
+++ Fragments/src/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import './App.css'
+
+function App() {
+  return (
+   <React.Fragment>
+      <h1>Healthy list</h1>
+      <ol>
+        <li> soyabean chunks</li>
+        <li> bananas</li>
+        <li> eggs</li>
+        <li> paneer</li>
+      </ol>
+   </React.Fragment>
+    )
+}
+
+export default App
