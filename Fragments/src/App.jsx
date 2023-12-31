4d4247bca38d8421310b4266092230727dc0171c
Om Khulbe <84437272+yashsharma2912@users.noreply.github.com>
2023-12-31 09:59:03 +0530

    Update App.jsx

--- Fragments/src/App.jsx
+++ Fragments/src/App.jsx
@@ -2,15 +2,17 @@ import React from 'react'
 import './App.css'
 
 function App() {
+  let listItems = ["soyabean chunks","paneer","eggs","bananas"];
   return (
    <React.Fragment>
-      <h1>Healthy list</h1>
-      <ol>
-        <li> soyabean chunks</li>
-        <li> bananas</li>
-        <li> eggs</li>
-        <li> paneer</li>
-      </ol>
+       <h1>Healthy foods</h1>
+       <ul className='list-group'>{listItems.map((item) =>
+       <li key={item} className='list-group-item'>
+         {item}
+       </li>
+       )}
+                   
+       </ul>
    </React.Fragment>
     )
 }
