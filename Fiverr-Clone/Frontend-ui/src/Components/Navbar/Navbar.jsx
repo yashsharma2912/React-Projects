6d2aac03c4a9c984367e47abfd9973265960a097
Om Khulbe <84437272+yashsharma2912@users.noreply.github.com>
2024-04-12 00:31:20 +0530

    navbar created

--- Fiverr-Clone/Frontend-ui/src/Components/Navbar/Navbar.jsx
+++ Fiverr-Clone/Frontend-ui/src/Components/Navbar/Navbar.jsx
@@ -1,13 +1,29 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { useState } from 'react';
+
 import "../../Components/Navbar/Navbar.scss";
+
 const Navbar = () => {
+  const[active,setActive] = useState(false);
+
+  const handleScroll = () => {
+    window.scrollY > 0 ? setActive(true) : setActive(false);
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
   return (
     <div className='navbar'>
       <div className="container">
-         <div className="topNav">
+         <div className={active ? "topNav active" : "topNav"}>
                <div className="left">
                  <h2>fiverr <span className='dot'>.</span></h2>
-                 <input type="text" placeholder='What services are you looking for today?' />
+               {active && ( <input type="text" placeholder='What services are you looking for today?' /> )}
                </div>
                <div className="right">
                    <span>Fiverr Pro</span>
@@ -19,18 +35,128 @@ const Navbar = () => {
                </div>
          </div>
          <hr />
-         <div className="botNav">
+        {active && (<div className="botNav">
             <span>Graphics & Design</span>
             <span>Programming & Tech</span>
             <span>Digital Marketing</span>
             <span>Video & Animation</span>
             <span>Writing & Translation</span>
             <span>Music & Audio</span>
-            <span>Music & Audio</span>
-            <span>Music & Audio</span>
-         </div>
+            <span>Business</span>
+            <span>Data</span>
+         </div> )
+         }
          <hr />
       </div>
+      <h1>yash</h1>
+      <h1>yash</h1>
+      <h1>yash</h1>
+      <h1>yash</h1>
+      <h1>yash</h1>
+      <h1>yash</h1>
+      <h1>yash</h1>
+      <h1>yash</h1>
+      <h1>yash</h1>
+      <h1>yash</h1>
+      <h1>yash</h1>
+      <h1>yash</h1>
+      <h1>yash</h1>
+      <h1>yash</h1>
+      <h1>yash</h1>
+      <h1>yash</h1>
+      <h1>yash</h1>
+      <h1>yash</h1>
+      <h1>yash</h1>
+      <h1>yash</h1>
+      <h1>yash</h1>
+      <h1>yash</h1>
+      <h1>yash</h1>
+      <h1>yash</h1>
+      <h1>yash</h1>
+      <h1>yash</h1>
+      <h1>yash</h1>
+      <h1>yash</h1>
+      <h1>yash</h1>
+      <h1>yash</h1>
+      <h1>yash</h1>
+      <h1>yash</h1>
+      <h1>yash</h1>
+      <h1>yash</h1>
+      <h1>yash</h1>
+      <h1>yash</h1>
+      <h1>yash</h1>
+      <h1>yash</h1>
+      <h1>yash</h1>
+      <h1>yash</h1>
+      <h1>yash</h1>
+      <h1>yash</h1>
+      <h1>yash</h1>
+      <h1>yash</h1>
+      <h1>yash</h1>
+      <h1>yash</h1>
+      <h1>yash</h1>
+      <h1>yash</h1>
+      <h1>yash</h1>
+      <h1>yash</h1>
+      <h1>yash</h1>
+      <h1>yash</h1>
+      <h1>yash</h1>
+      <h1>yash</h1>
+      <h1>yash</h1>
+      <h1>yash</h1>
+      <h1>yash</h1>
+      <h1>yash</h1>
+      <h1>yash</h1>
+      <h1>yash</h1>
+      <h1>yash</h1>
+      <h1>yash</h1>
+      <h1>yash</h1>
+      <h1>yash</h1>
+      <h1>yash</h1>
+      <h1>yash</h1>
+      <h1>yash</h1>
+      <h1>yash</h1>
+      <h1>yash</h1>
+      <h1>yash</h1>
+      <h1>yash</h1>
+      <h1>yash</h1>
+      <h1>yash</h1>
+      <h1>yash</h1>
+      <h1>yash</h1>
+      <h1>yash</h1>
+      <h1>yash</h1>
+      <h1>yash</h1>
+      <h1>yash</h1>
+      <h1>yash</h1>
+      <h1>yash</h1>
+      <h1>yash</h1>
+      <h1>yash</h1>
+      <h1>yash</h1>
+      <h1>yash</h1>
+      <h1>yash</h1>
+      <h1>yash</h1>
+      <h1>yash</h1>
+      <h1>yash</h1>
+      <h1>yash</h1>
+      <h1>yash</h1>
+      <h1>yash</h1>
+      <h1>yash</h1>
+      <h1>yash</h1>
+      <h1>yash</h1>
+      <h1>yash</h1>
+      <h1>yash</h1>
+      <h1>yash</h1>
+      <h1>yash</h1>
+      <h1>yash</h1>
+      <h1>yash</h1>
+      <h1>yash</h1>
+      <h1>yash</h1>
+      <h1>yash</h1>
+      <h1>yash</h1>
+      <h1>yash</h1>
+      <h1>yash</h1>
+      <h1>yash</h1>
+      <h1>yash</h1>
     </div>
   )
 }
