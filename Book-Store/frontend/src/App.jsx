c23203cd48ae42471fe421d8ddda304abe633cef
Om Khulbe <84437272+yashsharma2912@users.noreply.github.com>
2024-04-15 23:56:52 +0530

    check

--- Book-Store/frontend/src/App.jsx
+++ Book-Store/frontend/src/App.jsx
@@ -7,6 +7,7 @@ function App() {
     <>
     <h1>My Book Store</h1>
     <div className="bookcard">
+      <h1>Hiii this is yash Sharma</h1>
        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Perspiciatis, earum?
     </div>
     </>
