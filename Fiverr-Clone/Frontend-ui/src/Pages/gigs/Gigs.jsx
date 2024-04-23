fb261c206a094e1428e792978253cfa069a2a3ea
Om Khulbe <84437272+yashsharma2912@users.noreply.github.com>
2024-04-23 17:14:45 +0530

    commit

--- Fiverr-Clone/Frontend-ui/src/Pages/gigs/Gigs.jsx
+++ Fiverr-Clone/Frontend-ui/src/Pages/gigs/Gigs.jsx
@@ -1,6 +1,9 @@
-import React from 'react'
+import React, { useState } from 'react'
 import "../gigs/Gigs.scss"
 import Gigcard from './Gigcard';
+
+import { IoIosArrowDown } from "react-icons/io";
+
 
 const data = [
    {
@@ -37,9 +40,39 @@ const data = [
    },
 ];
 const Gigs = () => {
+   
+  const [open,setOpen] = useState(false);
+  const [sort,setSort] = useState("sales");
+
+  const reSort = (type) => {
+    setSort(type);
+    setOpen(false);
+  }
+
   return (
     <div className="gigs">
-        
+       <div className="topinfon">
+         <p>FIVERR {'>'} GRAPHICS & DESIGN {">"}</p>
+         <h1>AI Artists</h1>
+         <p>Explore the boundries of art and tech witg Fiverr's AI artists</p>
+        <div className="sort">
+         <div className="left">
+            <span>Budget</span> <input type="text" placeholder='min' />
+            <input type="text" placeholder='max' />
+            <button>Apply</button>
+         </div>
+         <div className="right">
+             <span>sort by </span>
+           {sort ==="sales" ? <p>Best Selling</p> :
+             <p>Newest</p> }
+             <IoIosArrowDown className='tit' onClick={() => setOpen(!open)}/>
+             {open && (<div className="rightlist">
+                  <span onClick={()=>reSort("sales")}>Best Selling</span>
+                  <span onClick={()=>reSort("createdAt")}>Newest</span>
+             </div>)}
+         </div>
+         </div>
+        </div> 
 
         <div className="cards">
             {data.map((item) => <Gigcard 
